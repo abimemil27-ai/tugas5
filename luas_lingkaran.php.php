6e6dc55fdebf88ba2af9f02623d68656fe003c86
abimemil27-ai <abim.emil27@smk.belajar.id>
2025-09-28 05:27:33 -0700

    first commit

--- luas_lingkaran.php.php
+++ luas_lingkaran.php.php
@@ -5,10 +5,3 @@ $luas = pi() * pow($jari, 2);
 
 echo "Luas Lingkaran (r=$jari) = $luas";
 ?>
-echo "# tugas-5" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:abimemil27-ai/tugas-5.git
-git push -u origin main
